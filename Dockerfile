01ba08fad6135b32031dacc487472e72fbac1963
HumbertoM10 <humberto_e10@hotmail.es>
2021-03-10 22:17:09 -0600

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM golang:1.16
+WORKDIR /pokeapi
+ENV GOPATH /go
+ENV PATH $GOPATH/bin:/usr/local/go/bin:$PATH
+EXPOSE 3000
+COPY * .
+RUN go build main.go
+CMD ["./main"]
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/HumbertoM10/pokemonapi/packages/handler"
+	"github.com/gorilla/mux"
+)
+
+// main is in charge of setting the port and executing the server's goroutine.
+func main() {
+	port := 3000
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/advantage", handler.Advantage).Methods("GET")
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: router,
+	}
+	defer srv.Close()
+
+	// The server is ran using a gorutine so it doesn't block
+	go func() {
+		log.Printf("Listening on port %d...\n", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Print(err)
+		}
+	}()
+
+	// Block execution until a sigint signal is recieved
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
